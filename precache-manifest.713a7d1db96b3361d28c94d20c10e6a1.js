87761d7121ada3b602bd38fb50e0801ecfbd0346
Seong-Myeong <57748493+Seong-Myeong@users.noreply.github.com>
2024-12-03 01:18:56 +0900

    Add files via upload

---
+++ precache-manifest.713a7d1db96b3361d28c94d20c10e6a1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43c39d840a408676fceb4ac794719da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b77603ad1a458b171ee",
+    "url": "/static/css/main.b9a8cede.chunk.css"
+  },
+  {
+    "revision": "34a33af1b1fc112d9077",
+    "url": "/static/js/2.a8fe3fde.chunk.js"
+  },
+  {
+    "revision": "b95080709ac12d0967db165d90e6c04a",
+    "url": "/static/js/2.a8fe3fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b77603ad1a458b171ee",
+    "url": "/static/js/main.20506bab.chunk.js"
+  },
+  {
+    "revision": "b0b4c202eed4b06a6339",
+    "url": "/static/js/runtime-main.167eaf22.js"
+  },
+  {
+    "revision": "40b92fb6e0e447ca7f0c7fddb6cec9c6",
+    "url": "/static/media/Pretendard-Bold.subset.40b92fb6.woff2"
+  },
+  {
+    "revision": "6dec4607c4804775486c0b22e1b96f4b",
+    "url": "/static/media/Pretendard-ExtraBold.subset.6dec4607.woff2"
+  },
+  {
+    "revision": "e55a007a88db0a7a45bbeedd0bc9ce77",
+    "url": "/static/media/Pretendard-Medium.subset.e55a007a.woff2"
+  },
+  {
+    "revision": "97213db5c578498e3bab52fb858e720a",
+    "url": "/static/media/Pretendard-Regular.subset.97213db5.woff2"
+  },
+  {
+    "revision": "3ba9cd1e8bcb4824f073865d2ed81d34",
+    "url": "/static/media/Pretendard-SemiBold.subset.3ba9cd1e.woff2"
+  }
+]);
